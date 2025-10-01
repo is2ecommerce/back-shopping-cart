7e2273883df14db6e6dd96720ff8de1cf241ebb2
jhonc <calderonjhondairo@gmail.com>
2025-10-01 10:48:16 -0500

    init

---
+++ src/main/java/ecommerce/cart/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package ecommerce.cart.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("Shopping Cart API")
+                .version("1.0")
+                .description("API documentation for the shopping cart service"));
+  }
+}
